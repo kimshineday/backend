eebfa46d5bf7cd579bc566e61ee63328e66bcc3f
KIM SHINEDAY <kimshineday99@gmail.com>
2025-06-19 07:17:14 +0900

    🔥 Django : boards 폴더 삭제
    알 수 없는 에러 해결을 위한 폴더 삭제
    -> articles 폴더로 대체

--- Django/django-set/boards/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Board
-@admin.register(Board)
-class BoardAdmin(admin.ModelAdmin):
-    pass
--- Django/django-set/boards/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# 게시글 구성 : 제목, 내용
-class Board(models.Model):
-    title = models.CharField(max_length=30)
-    content = models.TextField()
